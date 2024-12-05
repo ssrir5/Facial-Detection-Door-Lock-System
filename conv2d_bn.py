be8ab72264e6ecdd8e6c4d5407c032b0a5268dc1
ssrir5 <117416099+ssrir5@users.noreply.github.com>
2024-12-04 22:04:31 -0600

    Update conv2d_bn.py

--- conv2d_bn.py
+++ conv2d_bn.py
@@ -1,5 +1,3 @@
- # -*- coding: utf-8 -*-
-## for ECE479 ICC Lab2 Part3
 
 '''
 *Definition for 2 dimension convolution layer with batch normalization*
@@ -21,26 +19,20 @@ def conv2d_bn(x,
     bn_axis = 1 if K.image_data_format() == 'channels_first' else 3
     ##############################################################
 
-    ## TO DO Step1 : Apply a Conv 2D Keras Layer with all given parameters
-    # Your code goes here
+## Apply a Conv 2D Keras Layer 
     x = Conv2D(filters, kernel_size, strides, padding, use_bias=use_bias, name=name)(x)
-
-
-
 
     if not use_bias:
 
         bn_name = generate_layer_name('BatchNorm', prefix=name)
-        ## TO DO Step 2 : Apply a Batch Normalization Keras Layer with all given parameters
-        # Your code goes here
+        ## Apply a Batch Normalization Keras Layer 
         x = BatchNormalization(bn_axis, 0.995, 0.001, scale=False, name=bn_name)(x)
 
 
 
     if activation is not None:
         ac_name = generate_layer_name('Activation', prefix=name)
-        ## TO DO Step 3 : Apply an Activation Keras Layer with all given parameters
-        # Your code goes here
+        ## Apply an Activation Keras Layer
         x = Activation(activation, name=ac_name)(x)
 
 
